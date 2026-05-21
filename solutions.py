45e355d9fc754a28aae1d680673a802f5ed7abb4
genevievesu <genevievesuu@gmail.com>
2026-05-21 19:05:54 -0400

    done

--- solutions.py
+++ solutions.py
@@ -11,6 +11,10 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+    projected_total_sales = input("What is your projected amount of total sales?")
+    profit = int(projected_total_sales) * 0.23
+    message = f"Profit: ${profit:,.2f}"
+    print(message)
 
 
 def calculate_quotient_and_remainder():
@@ -22,6 +26,12 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
+    first_integer = input("Pick your first integer! ")
+    second_integer = input("Pick your second integer! ")
+    quotient = int(first_integer) // int(second_integer)
+    remainder = int(first_integer) % int(second_integer)
+    message = second_integer + " goes into " + first_integer + " a total of " + str(quotient) + " times with a remainder of " + str(remainder)
+    print(message)
 
 
 def calculate_miles_per_gallon():
@@ -35,6 +45,11 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
+    miles = input("How many miles have you driven?")
+    gas = input("How many gallons of gas have you used?")
+    mpg = int(miles) / int(gas)
+    message = f"Miles per gallon: {mpg:.1f}"
+    print(message)
 
 
 def align_text():
@@ -53,3 +68,19 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    price_one = input("Enter price #1: ")
+    price_one_value = float(price_one)
+    price_two = input("Enter price #2: ")
+    price_two_value = float(price_two)
+    price_three = input("Enter price #3: ")
+    price_three_value = float(price_three)
+  
+
+    print("Here are your prices!\n")
+    print(f"Price #1: ${price_one_value:>15.2f}")
+    print(f"Price #2: ${price_two_value:>15.2f}")
+    print(f"Price #3: ${price_three_value:>15.2f}")
+
+
+
+
